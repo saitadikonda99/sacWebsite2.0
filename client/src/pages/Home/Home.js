e289554da4c39cf6880dcba064caaed2d6fb6807
Deepak Reedy Gathpa <deepakreddygathpa@gmail.com>
2023-09-03 10:09:55 +0530

    stable

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
 import ScrollToTop from "react-scroll-to-top";
+import { Link as Scroll } from 'react-scroll';
 
 import NewNav from '../../components/NewNav/NewNav'; 
 import Footer from '../../components/Footer/Footer';
-import Testimonial from '../../components/Testimonial/Testimonial'; 
 
 import G20andSac from '../../Assets/Logos/G20andSac.png';
 import G20andSac2 from '../../Assets/Logos/G20andSac2.png';
@@ -16,16 +16,7 @@ import bck from '../../Assets/bck.jpg';
 import newVideo from '../../Assets/newVideo.mp4';  
 import AmarnadhDroneCompetition from '../../Assets/NewsImages/AmarnadhDroneCompetition.jpg'; 
 import PreBootCamp from '../../Assets/NewsImages/PreBootCamp.jpg';
-import CouncilMember_01 from '../../Assets/CouncilMembers/CouncilMember_01.png';
-import CouncilMember_02 from '../../Assets/CouncilMembers/CouncilMember_02.png';
-import CouncilMember_04 from '../../Assets/CouncilMembers/CouncilMember_04.png';
-import CouncilMember_06 from '../../Assets/CouncilMembers/CouncilMember_06.png';
-import CouncilMember_07 from '../../Assets/CouncilMembers/CouncilMember_07.png';
-import CouncilMember_08 from '../../Assets/CouncilMembers/CouncilMember_08.png';
-import CouncilMember_09 from '../../Assets/CouncilMembers/CouncilMember_09.png';
-import CouncilMember_10 from '../../Assets/CouncilMembers/CouncilMember_10.png';
-import CouncilMember_11 from '../../Assets/CouncilMembers/CouncilMember_11.png';
-import CouncilMember_12 from '../../Assets/CouncilMembers/CouncilMember_12.png';
+
 
 
 
@@ -44,103 +35,128 @@ const Home  = () => {
     };
   }, []);
 
- 
-  const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
-  const [isActive, setIsActive] = useState(false);
+  const [isNavScrolled, setNavScrolled] = useState(false);
 
-
-  function toggleOverlay() {
-    setIsActive(!isActive);
-  } 
-
+  // Event listener to handle scroll event
+  const handleScroll = () => {
+    if (window.scrollY > 500) {
+      setNavScrolled(true);
+    } else {
+      setNavScrolled(false);
+    }
+  };
 
   useEffect(() => {
-    window.onscroll = function() {
-      const currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-105px";
-      }
-      setPrevScrollpos(currentScrollPos);
+    // Add the scroll event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, [prevScrollpos]);
+  }, []);
+
+  
+
+
+  
 
 
   return (
     <div className='HomeComponent'>
 
         <div className="HomeContainer">
-               <div className="other">
-                  <img id='oldsac' className={isActive ? '' : 'hidden'} src={G20andSac2} alt=""  />
-                  <img id='newsac' className={isActive ? 'hidden' : ''} src={G20andSac} alt="" />
-               </div>
+          
+          <div className={`nav ${isNavScrolled ? 'scrolled' : ''}`}>
+            <div className="nav-in">
+              <div className="nav-in-one">
+                <div className="nav-in-one-in">
+                  <ul>
+                    <li>
+                      <Scroll
+                          activeClass="active"
+                          to="news"
+                          spy ={true}
+                          smooth={true}
+                          offset={100}
+                          duration={200}
+                        >   
+                        News
+                      </Scroll>  
+                    </li>
+                    <li>
+                      <Scroll
+                            activeClass="active"
+                            to="news-two"
+                            spy ={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={200}
+                          >   
+                          Events
+                        </Scroll>  
+                    </li>
+                    <li>
+                      <Scroll
+                            activeClass="active"
+                            to="about"
+                            spy ={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={200}
+                          >   
+                          About
+                        </Scroll>  
+                    </li>
+                    <li>
+                      <Link className='nav-links' to='/'>Clubs</Link>
+                    </li>
+                    <li>
+                      <Link className='nav-links'  to='/'>Team</Link>
+                    </li>
+                    <li>
+                      <Scroll
+                            activeClass="active"
+                            to="about"
+                            spy ={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={200}
+                          >   
+                          Student Council
+                        </Scroll>  
+                    </li>
+                    <li>
+                      <Scroll
+                            activeClass="active"
+                            to="about"
+                            spy ={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={200}
+                          >   
+                          News Letter
+                        </Scroll>  
+                    </li>
 
-               <div className='mainAnother'  id='navbar' >
-                  <div className="users">
-                    <i class="fa fa-user" aria-hidden="true"></i>
-                  </div>
-                  <div className={`button_container strides ${isActive ? 'active' : ''}`} onClick={toggleOverlay}>
-                    <span className="top"></span>
-                    <span className="middle"></span>
-                    <span className="bottom"></span>
-                  </div>
-               </div>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
+          <div className="Home-one">
+            <div className="Home-one-in">
+              <video autoPlay={true} loop={true} muted={true} style={{width: "100%"}} src={newVideo}></video>
+            </div>
+          </div>
 
                
-                <div className={`overlay ${isActive ? 'open' : ''}`} id="overlay">
-                    <NewNav/>
-                </div>
-                
-        <div className="videoContainer">
-            <video autoPlay={true} loop={true} muted={true} style={{width: "100%"}} src={newVideo}></video>
-            
-           <center>
-              <div className="bind">
-                <center>
-                  <h1>Student Activity Center</h1>
-                </center>
-              </div>
-           </center>
-
-            <a id="activit">
-            <div class="boxdown">
-              <div className="boxdown-one">
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </div>
-            </a>
-
-
-            
-
-           
-        </div>
-        <ScrollToTop  smooth id='scrollToTop' component={<p style={{ color: "rgb(151, 0, 3)" }}>Back to top</p>}></ScrollToTop>
+          <ScrollToTop  smooth id='scrollToTop' component={<p style={{ color: "rgb(151, 0, 3)" }}>Back to top</p>}></ScrollToTop>
 
 
 
-        <div id='activit' className="activities">
-          <div className="activities-heading">
-            <h1>Student Activity Center at KLEF</h1>
-          </div>
-          <div className="activities-inner">
-            <div className="activities-one">
-              <p>Home/<span id='dark'>The department of prominance</span> </p>
-              <h3>The focus areas at KL University's Student Activity Center benefit students by providing opportunities to explore interests, develop skills, <span className='underlin'>gain practical experience</span>, network with peers and industry professionals, <span className='underlin'>build teamwork</span> and leadership skills, and enhance communication skills.</h3>
-            </div>
-            <div className="activities-two">
-             <div className="activities-two-one">
-             <p>The Student Activity Centre (SAC) at KL University is committed to shaping students' intellectual and personal growth. By providing a platform that promotes engagement in activities beyond the traditional classroom setting, SAC aims to foster academic and social success. SAC strives to create an environment that encourages students to explore their interests and passions, develop new skills and knowledge, and broaden their horizons. In addition to facilitating social and educational growth, student clubs and activity centres offer students access to valuable resources, including state-of-the-art equipment, expert guidance, and mentoring from experienced professionals. SAC places a high emphasis on community service and volunteer work to help students develop a sense of social responsibility and empathy for others. </p>
-             </div>
-             <div className="activities-two-two">
-              <img src={GroupPhoto} alt="" />
-             </div>
-            </div>
-          </div>
-        </div>
 
 
       
@@ -261,91 +277,6 @@ const Home  = () => {
 
 
           
-
-
-            <div className="team">
-              <div className="team-heading">
-                <h1>Students Council</h1>
-              </div>
-              <div className="team-inner">
-                <div className="team-one">
-                      
-                      <div className="mem">
-                        <div className="mem-img">
-                          <img src={CouncilMember_06} alt="" />
-                        </div>
-                          <h3>Amarnadh </h3>
-                          <p>School of Automation</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_11} alt="" />
-                       </div>
-                        <h3>Amish Kumar </h3>
-                        <p>Photography</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_01} alt="" />
-                       </div>
-                        <h3>Arsene</h3>
-                        <p>Ohana</p>
-                      </div>
-                      <div className="mem">
-                       <div className="mem-img">
-                        <img src={CouncilMember_02} alt="" />
-                       </div>
-                        <h3>Deepak Reddy </h3>
-                        <p>The School of Coding</p>
-                      </div>
-                      
-                      
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_10} alt="" />
-                       </div>
-                        <h3>Hema </h3>
-                        <p>Safe Life</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_08} alt="" />
-                       </div>
-                        <h3>John Rahul </h3>
-                        <p>Standup Comedy</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_07} alt="" />
-                       </div>
-                        <h3>Lalith Kumar</h3>
-                        <p>Web Development</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_04} alt="" />
-                       </div>
-                        <h3>Navyatha Jallu</h3>
-                        <p>K L Radio</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_12} alt="" />
-                       </div>
-                        <h3>Parth Thakkar </h3>
-                        <p>K L Talks</p>
-                      </div>
-                      <div className="mem">
-                      <div className="mem-img">
-                        <img src={CouncilMember_09} alt="" />
-                       </div>
-                        <h3>Yasaswini</h3>
-                        <p>Muse Arts</p>
-                      </div>
-                      
-                </div>
-              </div>
-            </div>
 
 
 
@@ -517,8 +448,7 @@ const Home  = () => {
 
 
 
-            <Testimonial/>
-            
+
             
 
             <div id='qckid' className="quicklinks">
@@ -570,8 +500,6 @@ const Home  = () => {
                     </div>
               </div>
               <div className="quick-image">
-                {/* <a href="https://www.kluniversity.in/"><img src="https://i.ibb.co/YhMKv90/sac-logo-68af69157709144b2811.png" alt="" /></a> */}
-
                 <h1>Quick Links for activities at SAC</h1>
                 <div className="explore">
                   <Link className="exp">Deep Dive

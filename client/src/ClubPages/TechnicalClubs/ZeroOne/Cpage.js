a1516822ac16fcfc8c0594eac05811b09a5ae2a4
saitadikonda99 <saitadikonda.tech@gmail.com>
2023-09-05 22:42:30 +0530

    clubpage added

--- client/src/ClubPages/TechnicalClubs/ZeroOne/Cpage.js
+++ client/src/ClubPages/TechnicalClubs/ZeroOne/Cpage.js
@@ -7,6 +7,7 @@ import { BsFileEarmarkCode } from 'react-icons/bs';
 
 import deepakimg from '../../../Assets/NewsImages/PreBootCamp.jpg'
 import deepak from '../../../Assets/CouncilMembers/CouncilMember_02.png'
+import clubpage from '../../../Assets/clubpage.webp'
 import Footer from '../../../components/Footer/Footer'
 import Navbar from './NavBar'
 
@@ -29,81 +30,114 @@ var Page = () => {
 
 {/* ----------------------------------Hero-------------------------------------------- */}
 
+            <div className="club-hero">
+              <div className="club-hero-in">
+                </div>
+            </div>
 
-{/* ----------------------------------Team-------------------------------------------- */}
 
-              <div className="clubs-team">
-                <div className="clubs-team-in">
 
-                  <div className="clubs-team-in-one">
-                    <h1>Team</h1>
-                     </div>
+{/* ----------------------------------Main-------------------------------------------- */}
 
-                  <div className="clubs-team-in-two">
-                    <div className="member-box">
-                        <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
-                      </div>
-                  <div className="member-box">
-                    <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
-                  </div>
-                    <div className="member-box">
-                       <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
+
+          <div className="club-main-component">
+            <div className="club-main-component-in">
+
+                <div className="club-main-container-one">
+                  <div className="club-main-navbar">
+                    <div className="club-main-navbar-in">
+                      <div className="club-nav-button nav-button-one" 
+                                    id = { activity === 1 ? 'button-hover' : '' }
+                                    onClick={ () => {handleActivity(1)}}>Why you should join</div>
+                      <div className="club-nav-button nav-button-two" 
+                                    id = { activity === 2 ? 'button-hover' : '' }
+                                    onClick={ () => {handleActivity(2)}}>Glimpse</div>
+                      <div className="club-nav-button nav-button-three" 
+                                    id = { activity === 3 ? 'button-hover' : '' }
+                                    onClick={ () => {handleActivity(3)}}>Team</div>
+                      <div className="club-nav-button nav-button-four" 
+                                    id = { activity === 4 ? 'button-hover' : '' }
+                                    onClick={ () => {handleActivity(4)}}>Activities</div>
                     </div>
-                       </div>
-
-                  <div className="clubs-team-in-three">
-                  <div className="member-box">
-                        <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
-                      </div>
-                  <div className="member-box">
-                    <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
                   </div>
-                    <div className="member-box">
-                       <div className="member-name">
-                          <img src={deepak} alt="" />
-                        </div>
-                        <div className="member-details">
-                            <p>Deepak</p>
-                            <p>President ZeroOne</p>
-                        </div>
-                    </div>
                 </div>
 
+                <div className="club-main-container-two"></div>
+                    <div className="club-main-content">
+                      <div className="club-main-content-in">
+                        <div className = { activity === 1 ? 'club-main-content-box' : 'main-content-hide' } >
+                          
+                          <div className="club-box-content-one-head">
+                              <h1>Why you should join the club!</h1>
+                          </div>
+                            <div className="club-box-content-one">
+                                  <div className="content-one-matter">
+                                    <div className="one-matter-img">
+                                      <img src={clubpage} alt="" />
+                                    </div>
+                                    <div className="one-matter-img-content">
+                                      <h1>Exclusive Events</h1>
+                                      <p>Get access to LIVE events that help you explore your favorite fields and grow to the next level.</p>
+                                    </div>
+                                  </div>
+                                  <div className="content-two-matter">
+                                  <div className="one-matter-img">
+                                      <img src={clubpage} alt="" />
+                                    </div>
+                                    <div className="one-matter-img-content">
+                                      <h1>Exclusive Events</h1>
+                                      <p>Get access to LIVE events that help you explore your favorite fields and grow to the next level.</p>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div className="club-box-content-two">
+                                  <div className="content-three-matter">
+                                  <div className="one-matter-img">
+                                      <img src={clubpage} alt="" />
+                                    </div>
+                                    <div className="one-matter-img-content">
+                                      <h1>Exclusive Events</h1>
+                                      <p>Get access to LIVE events that help you explore your favorite fields and grow to the next level.</p>
+                                    </div>
+                                  </div>
+
+                                  <div className="content-four-matter">
+                                  <div className="one-matter-img">
+                                      <img src={clubpage} alt="" />
+                                    </div>
+                                    <div className="one-matter-img-content">
+                                      <h1>Exclusive Events</h1>
+                                      <p>Get access to LIVE events that help you explore your favorite fields and grow to the next level.</p>
+                                    </div>
+                                  </div>
+                            </div>
+
+                          </div>
+                        <div className = { activity === 2 ? 'club-main-content-box' : 'main-content-hide' } >BOX-2</div>
+                        <div className = { activity === 3 ? 'club-main-content-box' : 'main-content-hide' } >BOX-3</div>
+                        <div className = { activity === 4 ? 'club-main-content-box' : 'main-content-hide' } >BOX-4</div>
+                        </div>
+                    </div>
               </div>
-            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
               
 
-{/* ----------------------------------Activities-------------------------------------------- */}
+{/* ----------------------------------Activities--------------------------------------------
 
 
               <div className="club-activity-component">
@@ -113,28 +147,28 @@ var Page = () => {
                   </div>
                 <div className="clubs-Activities-in">
                   <div className="clubs-Activities-in-one">
-                          <div className = { activity === 1 ? 'activity-content-one' : 'activity-hide' } id='one-color'></div>
-                          <div className = { activity === 2 ? 'activity-content-one' : 'activity-hide' } id='two-color'></div>
-                          <div className = { activity === 3 ? 'activity-content-one' : 'activity-hide' } id='three-color'></div>
-                          <div className = { activity === 4 ? 'activity-content-one' : 'activity-hide' } id='four-color'></div>
+                          <div className = { activity === 1 ? 'club-main-content-box' : 'main-content-hide' } id='one-color'></div>
+                          <div className = { activity === 2 ? 'club-main-content-box' : 'main-content-hide' } id='two-color'></div>
+                          <div className = { activity === 3 ? 'club-main-content-box' : 'main-content-hide' } id='three-color'></div>
+                          <div className = { activity === 4 ? 'club-main-content-box' : 'main-content-hide' } id='four-color'></div>
                      </div>
                       <div className="clubs-Activities-in-two">
-                         <div id = { activity === 1 ? 'activity-content-hover' : '' }
+                         <div id = { activity === 1 ? 'button-hover' : '' }
                           className="Activity-box-one" onClick={() => handleActivity(1)}>Activity-1</div>
-                         <div id = { activity === 2 ? 'activity-content-hover' : '' }
+                         <div id = { activity === 2 ? 'button-hover' : '' }
                          className="Activity-box-one" onClick={() => handleActivity(2)}>Activity-2</div>
-                         <div id = { activity === 3 ? 'activity-content-hover' : '' }
+                         <div id = { activity === 3 ? 'button-hover' : '' }
                          className="Activity-box-one" onClick={() => handleActivity(3)}>Activity-3</div>
-                         <div id = { activity === 4 ? 'activity-content-hover' : '' }
+                         <div id = { activity === 4 ? 'button-hover' : '' }
                          className="Activity-box-one" onClick={() => handleActivity(4)}>Activity-4</div>
                     </div>
                 </div>
               </div>
             </div>
 
-
+*/}
           </div>
-        </div>
+        </div>  
 
         <div className="club-footer">
           <div className="club-footer-in">

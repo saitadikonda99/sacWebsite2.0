fe1d69950acbfe2fefc13cd3e677f4269753de3b
Deepak Reedy Gathpa <deepakreddygathpa@gmail.com>
2023-09-09 11:05:08 +0530

    9th sep first push

--- client/src/components/Navigation/Page.js
+++ client/src/components/Navigation/Page.js
@@ -61,9 +61,11 @@ const Page = () => {
                     <li><Scroll className='nav-scroll-links' activeClass="active" to="projects" spy ={true} smooth={true} offset={-50} duration={200}>Clubs</Scroll></li>
                     <li><Scroll className='nav-scroll-links' activeClass="active" to="stories" spy ={true} smooth={true} offset={-50} duration={200}>Blogs</Scroll></li>
                     <li><Scroll className='nav-scroll-links' activeClass="active" to="news" spy ={true} smooth={true} offset={-60} duration={200} >News</Scroll></li>
-                    <li><Link className='nav-links'  to='/leadership'>Leadership</Link></li>
-                    <li><Link className='nav-links' to='/StudentClubs'>Gallery</Link></li>
-                    <li><Scroll className='nav-scroll-links' activeClass="active" to="" spy ={true} smooth={true} offset={-50} duration={200} onClick={handleClickOpen}>Notifications</Scroll></li>
+                    <li><Scroll className='nav-scroll-links' activeClass="active" to="upcoming-events" spy ={true} smooth={true} offset={-60} duration={200} >Events</Scroll></li>
+                    {/* <li><Link className='nav-links' to='/StudentClubs'>Gallery</Link></li> */}
+                    <li><Scroll className='nav-scroll-links' activeClass="active" to="follow" spy ={true} smooth={true} offset={-60} duration={200} >Social Media</Scroll></li>
+                    <li><Scroll className='nav-scroll-links' activeClass="active" to="faq" spy ={true} smooth={true} offset={-60} duration={200} >FAQ</Scroll></li>
+                    {/* <li><Scroll className='nav-scroll-links' activeClass="active" to="" spy ={true} smooth={true} offset={-50} duration={200} onClick={handleClickOpen}>Notifications</Scroll></li> */}
                   </ul>
                 </div>
               </div>
